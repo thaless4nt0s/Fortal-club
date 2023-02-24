692809a35f49701281e7aefb84030326ad038b11
= <=>
2023-02-24 15:12:58 -0300

    Correção migration de socios

---
+++ src/database/migrations/20230224173435-create-socios-table.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("socios",{
+      id:{
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      nome:{
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING
+      },
+      email:{
+        allowNull: false,
+        unique: true,
+        type: Sequelize.DataTypes.STRING
+      },
+      senha:{
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable("socios");
+  }
+};
