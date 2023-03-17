c2797b5899069714fe5ba682a93fad77521907d0
= <=>
2023-03-17 17:59:33 -0300

    Correção da tabela de dependentes

---
+++ src/database/migrations/20230317205029-create-depend-table.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("deps", {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      nome: {
+        allowNoll: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      socio_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+        references: { model: "socios", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "RESTRICT",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("deps");
+  },
+};
