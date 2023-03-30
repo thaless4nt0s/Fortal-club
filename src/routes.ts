f6b777918f5ba3ab97f238fea7ea95e6c75409e7
= <=>
2023-03-30 10:10:27 -0300

    rota para criar dependente

--- src/routes.ts
+++ src/routes.ts
@@ -7,10 +7,11 @@ import { depoimentosController } from "./controllers/depoimentosController";
 
 const router = express.Router();
 
-router.post("/auth/register", sociosController.register);
+router.post("/auth/register", sociosController.create);
 router.post("/auth/login", authController.login);
 router.post("/veiculos", veiculosController.create);
 router.post("/depoimentos", depoimentosController.create);
+router.post("/dependentes", dependentesController.create);
 
 router.get("/socios", sociosController.index);
 router.get("/socios/:id", sociosController.show);
@@ -20,7 +21,7 @@ router.get("/veiculos", veiculosController.index);
 router.get("/depoimentos/all", depoimentosController.index);
 router.get("/depoimentos", depoimentosController.depoimentoDeUmSocio);
 
-router.delete("/socios/", sociosController.remover);
+router.delete("/socios", sociosController.delete);
 router.delete("/dependentes/:id", dependentesController.delete);
 router.delete("/veiculos/:id", veiculosController.delete);
 router.delete("/depoimentos/:id", depoimentosController.delete);
@@ -28,5 +29,6 @@ router.delete("/depoimentos/:id", depoimentosController.delete);
 router.put("/dependentes/:id", dependentesController.update);
 router.put("/veiculos/:id", veiculosController.update);
 router.put("/depoimentos/:id", depoimentosController.update);
+router.put("/socios", sociosController.update);
 
 export { router };
